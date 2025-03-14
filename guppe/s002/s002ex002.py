281e91b22026a74b5ec8b18eebd275555b3499b3
taiane94sb <taiane19sb94@gmail.com>
2025-03-14 09:08:05 -0300

    Dir e Help

---
+++ guppe/s002/s002ex002.py
@@ -0,0 +1,19 @@
+"""
+Utilitários Python que auxiliam na programação.
+
+Dir - Apresenta todos os atributos/propriedades e funções/métodos disponíveis para determinado tipo de dado/variável.
+
+dir(tipo de dado/variável)
+dir("Taiane")
+
+
+Help - Apresenta a documentação de como utilizar os atributos/propriedades e funções/métodos disponíveis para
+determinado tipo de dado/variável.
+
+help(tipo de dado/variável.propriedade)
+help("Taiane".lower)
+"""
+
+
+print(dir("Taiane"))
+help("Taiane".upper)
