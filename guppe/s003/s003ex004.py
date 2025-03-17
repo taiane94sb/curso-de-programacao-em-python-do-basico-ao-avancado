5779d8103d921af8235851565e5848d9ea4edfcf
taiane94sb <taiane19sb94@gmail.com>
2025-03-17 20:34:41 -0300

    O tipo string

---
+++ guppe/s003/s003ex004.py
@@ -0,0 +1,37 @@
+"""
+Tipo string
+"""
+
+
+nome = "Taiane Barbosa"
+print(nome)
+print(type(nome))
+
+nome = "Gina's Bar"
+print(nome)
+print(type(nome))
+
+nome = 'Angelina Jolie'
+print(nome)
+print(type(nome))
+
+nome = 'Angelina \nJolie'
+print(nome)
+print(type(nome))
+
+nome = """Angelina
+Jolie"""
+print(nome)
+print(type(nome))
+
+ #      0123456789
+nome = "Taiane Barbosa"
+print(nome)
+print(nome.upper())
+print(nome.lower())
+print(nome.split())
+print(nome[0:6])
+print(nome[7:14])
+print(nome[::-1])
+print(nome)
+print(nome.replace('T', 'M'))
