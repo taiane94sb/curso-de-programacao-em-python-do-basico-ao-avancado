91d16bfbc5dbe506a79bba02521fcaa29d421404
taiane94sb <taiane19sb94@gmail.com>
2025-03-17 20:56:53 -0300

    Exercícios para praticar

---
+++ guppe/s003/s003ex006.py
@@ -0,0 +1,8 @@
+"""
+1. Faça um programa que leia um número inteiro e imprima-o.
+"""
+
+
+numero: int = int(input("Informe um numero inteiro: "))
+
+print(numero)
---
+++ guppe/s003/s003ex007.py
@@ -0,0 +1,12 @@
+"""
+2. Faça um programa que peça para o usuário digitar três valores inteiro e imprima a soma deles.
+"""
+
+
+numero1: int = int(input('Informe o primeiro inteiro: '))
+numero2: int = int(input('Informe o segundo inteiro: '))
+numero3: int = int(input('Informe o terceiro inteiro: '))
+
+soma: int = numero1 + numero2 + numero3
+
+print(f"A soma dos numeros {numero1} com {numero2} e {numero3} eh {soma}")
