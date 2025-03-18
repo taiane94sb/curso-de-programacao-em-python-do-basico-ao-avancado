a1f2b8cfc3fce694cdc5eec8fb636bf04a7e8eed
taiane94sb <taiane19sb94@gmail.com>
2025-03-18 13:51:38 -0300

    If, else, elif

---
+++ guppe/s004/s004ex001.py
@@ -0,0 +1,12 @@
+"""
+Estruturas condicionais
+if (se), else, elif
+"""
+
+
+idade = 18
+
+if idade < 18:
+    print("Menor de idade.")
+else:
+    print("Maior de idade.")
---
+++ guppe/s004/s004ex002.py
@@ -0,0 +1,14 @@
+"""
+Estruturas condicionais
+if (se), else, elif
+"""
+
+
+idade = 18
+
+if idade < 18:
+    print("Menor de idade.")
+elif idade == 18:
+    print("Tem 18 anos.")
+else:
+    print("Maior de idade.")
